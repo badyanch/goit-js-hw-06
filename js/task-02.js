29a9bbc53cbdae47ff01ce8db7d2f01e747b6ca6
Bohdan Zlotko <bohdanzlotko@gmail.com>
2023-03-22 09:14:33 +0200

    add task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listIngr = document.querySelector("ul#ingredients");
+const listItemsEl = ingredients.map((label) => {
+  const itemEl = document.createElement("li");
+
+  itemEl.textContent = label;
+  itemEl.classList.add("item");
+
+  return itemEl;
+});
+listIngr.append(...listItemsEl);
