b4dbbe7052961ccabc70e87009c20bfd2bbce762
Bohdan Zlotko <bohdanzlotko@gmail.com>
2023-03-23 16:14:08 +0200

    add task 3

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,48 @@ const images = [
     alt: "Group of Horses Running",
   },
 ];
+
+const galleryRef = document.querySelector("ul.gallery");
+
+const markup = images.map((img) => `<li><img src="${img.url}" alt="${img.alt}" /></li>`).join("");
+
+galleryRef.insertAdjacentHTML("afterbegin", markup);
+
+// style
+const listStylesGallery = {
+  display: "grid",
+  listStyle: "none",
+  gridTemplateColumns: "1fr 1fr 1fr",
+  gap: "40px",
+  margin: "0",
+  padding: "0",
+};
+
+const nameStylesGallery = Object.keys(listStylesGallery);
+
+nameStylesGallery.forEach(
+  (nameListStyle) =>
+    (galleryRef.style[nameListStyle] = listStylesGallery[nameListStyle])
+);
+
+// style img
+const imgStylesGallery = {
+  display: "block",
+  height: "auto",
+  width: "100%",
+};
+
+const itemsGalleryEl = galleryRef.children;
+
+const nameImgStylesGallery = Object.keys(imgStylesGallery);
+
+for (const itemEl of itemsGalleryEl) {
+  itemEl.style.margin = "auto";
+
+  const imgEl = itemEl.querySelector("img");
+
+  nameImgStylesGallery.forEach(
+    (nameImgStyles) =>
+      (imgEl.style[nameImgStyles] = imgStylesGallery[nameImgStyles])
+  );
+}
